f025445829b7c0a88c955d088c3232ac6bb011da
김보성 <ghalsru@naver.com>
2019-03-17 22:20:25 +0900

    서버 페이지 확인용으로 변경

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '서버 연결 확인' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+// User info
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// User login
+router.get('/login', function(req, res){
+  res.send('get /login');
+});
+
+// User 
+router.get('/login', function(req, res){
+  res.send('get /login');
+});
+
 
 module.exports = router;
