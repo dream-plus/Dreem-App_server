35bd70096f3838a3edd3c5151d70ee462c5820b0
김보성 <ghalsru@naver.com>
2019-03-21 14:49:05 +0900

    Add : package, etc..

--- function/users/funUsers.js
+++ function/users/funUsers.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mysql_dbc = require('../../config/db_con')();
+var connection = mysql_dbc.init();
+var bcrypt = require('bcrypt');
 
 fn = {}
+
 // userSignup
 fn.userSignup =  function (req, res, next) {
     var sql = 'SELECT * FROM customer_info' ;
