f867200c76a6fafcd773daafd0e8bce61e398cf9
김보성 <ghalsru@naver.com>
2019-03-26 22:55:47 +0900

    Add : database test용 업로드

--- function/users/funUsers.js
+++ function/users/funUsers.js
@@ -1,10 +1,39 @@
 var express = require('express');
-var router = express.Router();
 var mysql_dbc = require('../../config/db_con')();
 var connection = mysql_dbc.init();
-var bcrypt = require('bcrypt');
+// var bcrypt = require('bcrypt');
+
+const session = require('express-session');
+const FileStore = require('session-file-store')(session); 
 
 fn = {}
+
+// For testing database connections 
+fn.usertest =  function (req, res, next) {
+  var sql = 'SELECT * FROM customer_info' ;  // customer_info 부분에 너가 만든 table로 변경해서 하면돼.
+
+  connection.query(sql,function(err, result) {
+    if(!err){
+      res.send(result);
+    } else {
+      console.log('Error');
+      res.render('index', {title:'Error'});
+    }
+  });
+}
+
+// check user session
+fn.userSession = function( req, res, next){
+  console.log(req.session);
+
+  if(!req.session.num){
+    req.session.num = 1;
+  } else {
+    req.session.num = req.session.num + 1;
+  }
+  res.send(`Number : ${req.session.num}`);
+
+}
 
 // userSignup
 fn.userSignup =  function (req, res, next) {
@@ -18,6 +47,8 @@ fn.userSignup =  function (req, res, next) {
         res.render('index', {title:'Error'});
       }
     });
-  }
+}
+
+
 
 module.exports = fn;
--- routes/users.js
+++ routes/users.js
@@ -2,22 +2,30 @@ var express = require('express');
 var router = express.Router();
 var funUsers = require('../function/users/funUsers');
 
+// test only
+let test = {};
+
 // User info
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(test)
+  // funUsers.userSession(req, res, next);
+  funUsers.usertest(req, res, next);
+  // res.send(test);
 });
 
 // User signup
 router.post('/signup', function(req, res){
   console.log(req.body);
-  funUsers.userSignup(req, res, next);
-  // res.json({success: true, msg: 'signin success'});
+  // funUsers.userSignup(req, res, next);
+  test = req.body;
+  res.json({success: true, msg: 'signup success'});
 });
 
 // User signin
 router.post('/signin', function(req, res){
   console.log(req.body);
-  res.send('get /login');
+  test = req.body;
+  res.json({success: true, msg: 'signin success'});
 });
 
 // User delete
