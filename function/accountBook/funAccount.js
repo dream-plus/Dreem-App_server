2235bfcf9d0e119d0e585254488afb5f4b4ef525
김보성 <ghalsru@naver.com>
2019-04-19 04:16:38 +0900

    Add : accountCall 을 세부적으로 나눔

--- function/accountBook/funAccount.js
+++ function/accountBook/funAccount.js
@@ -5,36 +5,90 @@ var connection = mysql_dbc.init();
 fn = {}
 
 fn.accountCall = function (req, res, next) {
-    const sql = `SELECT * FROM import`
 
-    connection.query(sql, function(err, data) {
-        if(!err){
-        //    console.log('data value = ' + data);
-           res.send(data);
-        // res.json({success: true, datavalue: data});
-        } else {
-          console.log('Error');
-          res.render('index', {title:'Error'});
-        }
-      });
+    if(req.params.id == "income"){
+        var sql = `SELECT * FROM import`
+
+        connection.query(sql, function(err, data) {
+            if(!err){
+            res.send(data);
+            } else {
+            console.log('Error');
+            res.render('index', {title:'Error'});
+            }
+        });
+    }else if(req.params.id == "expense"){
+        var sql = `SELECT * FROM expense`
+
+        connection.query(sql, function(err, data) {
+            if(!err){
+            res.send(data);
+            } else {
+            console.log('Error');
+            res.render('index', {title:'Error'});
+            }
+        });
+    }else if(req.params.id == "amount"){
+        var im_sql = `SELECT SUM(Import_amount) as imAmount FROM import`
+        var ex_sql = `SELECT SUM(Expense_amount) as exAmount FROM expense`
+
+        connection.query(im_sql,function(err, imdata){
+            if(!err){
+                connection.query(ex_sql,function(err, exdata){
+                    if(!err){
+                        // console.log(imdata[0].imAmount - exdata[0].exAmount);
+                        var result = imdata[0].imAmount - exdata[0].exAmount;
+                        return res.send({data: result});
+                    }else {
+                        console.log('Error');
+                        res.render('index', {title:'Error'});
+                        }
+                });
+            }else {
+                console.log('Error');
+                res.render('index', {title:'Error'});
+                }
+            
+        });
+    }
+    
 }
 
 /* account value add */
 fn.accountAdd =  function (req, res, next) {
-    var sql_insert = 'INSERT INTO import ( Details_of_usage, Import_amount, title, based) VALUES(?,?,?,?)';
-    params = [ req.body.detail , req.body.amount, req.body.title, req.body.based];
+
+    if(req.body.selection == "수입"){
+        var sql_insert = 'INSERT INTO import ( Details_of_usage, Import_amount, title, based) VALUES(?,?,?,?)';
+        params = [ req.body.detail , req.body.amount, req.body.title, req.body.based];
   
-    connection.query(sql_insert,params, function (err, result) {
+         connection.query(sql_insert,params, function (err, result) {
         
-       if(err){
-         console.log(err);
-          res.json({success: false, msg: err});
+        if(err){
+             console.log(err);
+             res.json({success: false, msg: err});
         }
         else{
           console.log('장부에 내용이 추가 되었습니다.');
           res.json({success: true, msg: 'add success'});
         }
       });
+    }else if(req.body.selection == "지출"){
+        var sql_insert = 'INSERT INTO expense ( Details_of_usage, Expense_amount, title, based) VALUES(?,?,?,?)';
+        params = [ req.body.detail , req.body.amount, req.body.title, req.body.based];
+  
+         connection.query(sql_insert,params, function (err, result) {
+        
+        if(err){
+             console.log(err);
+             res.json({success: false, msg: err});
+        }
+        else{
+          console.log('장부에 내용이 추가 되었습니다.');
+          res.json({success: true, msg: 'add success'});
+        }
+      });
+    }
+    
   }
 
 module.exports = fn;
--- routes/account.js
+++ routes/account.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var funAccount = require('../function/accountBook/funAccount');
 
 // call to db infor
-router.get('/call', function(req, res, next) {
+router.get('/call/:id', function(req, res, next) {
     funAccount.accountCall(req, res, next);
 });
 
 // insert to account data
 router.post('/add', function(req, res, next) {
+    console.log(req.body);
     funAccount.accountAdd(req, res, next);
 });
 
