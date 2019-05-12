80dd68ebdd72e182ec732d083253b5b5b2b9af9e
김보성 <ghalsru@naver.com>
2019-05-13 01:12:37 +0900

    Change : db 쿼리 변경

--- function/calendar/funCalendar.js
+++ function/calendar/funCalendar.js
@@ -1,15 +1,16 @@
 /*
 *   
 */
-
-var express = require('express');
+var mysql_dbc = require('../../config/db_con')();
+var connection = mysql_dbc.init();
 
 fn = {}
+
 // insert to database
 fn.writeCalendar =  function (req, res, next) {
-    const sql_insert = 'INSERT INTO calender_info (_id, name, title, contents, date, time, place, public, notice, member) VALUES(?,?,?,?,?,?,?,?,?,?)' ;
-    params = [req.body._id, req.body.name, req.body.title, req.body.contents, 
-      req.body.date, req.body.time, req.body.place, req.body.public, req.body.notice, req.body.member ];
+
+    var sql_insert = 'INSERT INTO calendar_info (_id, name, title, contents, date, time, place, public, notice, member) VALUES(?,?,?,?,?,?,?,?,?,?)' ;
+    params = [req.body._id, req.body.name, req.body.title, req.body.contents, req.body.date, req.body.time, req.body.place, req.body.public, req.body.notice, req.body.member ];
 
     connection.query(sql_insert,params,function(err, result) {
       if(!err){
